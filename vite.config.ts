a3d0541e68ed176c3753aa2fbf6f8057048e9a76
ziaddiaa01 <98720925+ziaddiaa01@users.noreply.github.com>
2024-12-11 05:44:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import githubPages from '@vitejs/plugin-react'; 
 
 
 export default defineConfig({
-  plugins: [react() , githubPages()],
-  base: '/bosta/',
+  plugins: [react()],
+  base: '/bosta',
   server: {
     proxy: {
       '/shipments': {

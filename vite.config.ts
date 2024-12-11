ac31696d646651bae3966f2cf76c756954b51441
$USER <$USER@$HOSTNAME>
2024-12-11 05:20:05 +0200

    add final version

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import githubPages from '@vitejs/plugin-react'; 
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() , githubPages()],
   base: '/bosta/',
   server: {
     proxy: {
